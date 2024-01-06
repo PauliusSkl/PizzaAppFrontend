7cdc02c7b7f29f34ace7a2753bd6b1f1f11eb7e8
Paulius Skliaudys <paulius.skliaudys9@gmail.com>
2024-01-06 22:39:33 +0200

    Minor change

--- src/CustomHooks/useFetchData.js
+++ src/CustomHooks/useFetchData.js
@@ -1,30 +1,5 @@
 import { useState, useEffect } from "react";
 
-const useFetchSizes = (url) => {
-  const [sizes, setSizes] = useState([]);
-
-  useEffect(() => {
-    fetch(`https://king-prawn-app-dndfk.ondigitalocean.app` + url)
-      .then((response) => response.json())
-      .then((data) => setSizes(data))
-      .catch((error) => console.error("Error:", error));
-  }, [url]);
-
-  return sizes;
-};
-
-const useFetchToppings = (url) => {
-  const [toppings, setToppings] = useState([]);
-
-  useEffect(() => {
-    fetch(`https://king-prawn-app-dndfk.ondigitalocean.app` + url)
-      .then((response) => response.json())
-      .then((data) => setToppings(data))
-      .catch((error) => console.error("Error:", error));
-  }, [url]);
-  return toppings;
-};
-
 const useFetchData = (url) => {
   const [data, setData] = useState([]);
 
@@ -37,4 +12,4 @@ const useFetchData = (url) => {
   return data;
 };
 
-export { useFetchSizes, useFetchToppings, useFetchData };
+export { useFetchData };
